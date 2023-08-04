c80131148931ba575c344a1b7cc9d73d182c8865
Altuğ Bilgin ALTINTAŞ <altuga@gmail.com>
2023-08-04 11:13:52 +0000

    Repo added

--- Repo.java
+++ Repo.java
@@ -3,7 +3,8 @@
 public class Repo  {
 
     public static void main(String[] args) {
-        
+           int value= Runtime.getRuntime().availableProcessors();
+           System.out.println(" " + value);
     }
 
     
